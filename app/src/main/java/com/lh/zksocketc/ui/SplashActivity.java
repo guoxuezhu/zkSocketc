bcce1acd96d8fd893a494b8950888e473d133828
guoxuezhu <1174327885@qq.com>
2019-03-22 13:28:44 +0800

    fix 升级

--- app/src/main/java/com/lh/zksocketc/ui/SplashActivity.java
+++ app/src/main/java/com/lh/zksocketc/ui/SplashActivity.java
@@ -51,6 +51,15 @@ public class SplashActivity extends BaseActivity {
         startActivity(new Intent(Settings.ACTION_SETTINGS));
     }
 
+    @OnClick(R.id.updata_seting)
+    public void updata_seting() {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("*/*");//设置类型，我这里是任意类型，任意后缀的可以这样写。
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        startActivityForResult(intent,1);
+
+    }
+
 
 
     @OnClick(R.id.admin_seting)
