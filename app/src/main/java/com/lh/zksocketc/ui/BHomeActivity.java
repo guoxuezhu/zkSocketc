b3676c5022a733c7302edaba8efdc1dc68babdf7
gxz <1174327885@qq.com>
2019-07-22 14:45:47 +0800

    fix

--- app/src/main/java/com/lh/zksocketc/ui/BHomeActivity.java
+++ app/src/main/java/com/lh/zksocketc/ui/BHomeActivity.java
@@ -19,16 +19,16 @@ import butterknife.OnClick;
 
 public class BHomeActivity extends BaseActivity {
 
-    @BindView(R.id.tv_wsd_wd)
-    TextView tv_wsd_wd;
-    @BindView(R.id.tv_wsd_sd)
-    TextView tv_wsd_sd;
-    @BindView(R.id.tv_wsd_pm)
-    TextView tv_wsd_pm;
+    @BindView(R.id.B_tv_wsd_wd)
+    TextView B_tv_wsd_wd;
+    @BindView(R.id.B_tv_wsd_sd)
+    TextView B_tv_wsd_sd;
+    @BindView(R.id.B_tv_wsd_pm)
+    TextView B_tv_wsd_pm;
 
 
-    @BindView(R.id.ckbox_kt_open_close)
-    CheckBox ckbox_kt_open_close;
+    @BindView(R.id.B_ckbox_kt_open_close)
+    CheckBox B_ckbox_kt_open_close;
 
     Handler smHander = new Handler() {
         @Override
@@ -58,57 +58,57 @@ public class BHomeActivity extends BaseActivity {
     private void initView() {
         WsdDataDao wsdDataDao = MyApplication.getDaoSession().getWsdDataDao();
         if (wsdDataDao.loadAll().size() == 0) {
-            tv_wsd_wd.setText("");
-            tv_wsd_sd.setText("");
-            tv_wsd_pm.setText("");
+            B_tv_wsd_wd.setText("");
+            B_tv_wsd_sd.setText("");
+            B_tv_wsd_pm.setText("");
         } else {
-            tv_wsd_wd.setText(wsdDataDao.loadAll().get(0).wendu);
-            tv_wsd_sd.setText(wsdDataDao.loadAll().get(0).shidu);
-            tv_wsd_pm.setText(wsdDataDao.loadAll().get(0).pm25);
+            B_tv_wsd_wd.setText(wsdDataDao.loadAll().get(0).wendu);
+            B_tv_wsd_sd.setText(wsdDataDao.loadAll().get(0).shidu);
+            B_tv_wsd_pm.setText(wsdDataDao.loadAll().get(0).pm25);
         }
 
     }
 
-    @OnClick(R.id.rbtn_shangke)
-    public void rbtn_shangke() {
+    @OnClick(R.id.B_rbtn_shangke)
+    public void B_rbtn_shangke() {
         SerialPortUtil.sendMsg("MBS1");
     }
 
 
-    @OnClick(R.id.rbtn_xiake)
-    public void rbtn_xiake() {
+    @OnClick(R.id.B_rbtn_xiake)
+    public void B_rbtn_xiake() {
         SerialPortUtil.stopWSDmsg(smHander);
         SerialPortUtil.sendMsg("MBS2");
         startActivity(new Intent(this, SplashActivity.class));
         finish();
     }
 
-    @OnClick(R.id.rbtn_cl_open)
-    public void rbtn_cl_open() {
+    @OnClick(R.id.B_rbtn_cl_open)
+    public void B_rbtn_cl_open() {
         SerialPortUtil.sendMsg("MBS5");
     }
 
 
-    @OnClick(R.id.rbtn_cl_close)
-    public void rbtn_cl_close() {
+    @OnClick(R.id.B_rbtn_cl_close)
+    public void B_rbtn_cl_close() {
         SerialPortUtil.sendMsg("MBS6");
     }
 
 
-    @OnClick(R.id.rbtn_dg_open)
-    public void rbtn_dg_open() {
+    @OnClick(R.id.B_rbtn_dg_open)
+    public void B_rbtn_dg_open() {
         SerialPortUtil.sendMsg("MBS13");
     }
 
-    @OnClick(R.id.rbtn_dg_close)
-    public void rbtn_dg_close() {
+    @OnClick(R.id.B_rbtn_dg_close)
+    public void B_rbtn_dg_close() {
         SerialPortUtil.sendMsg("MBS14");
 
     }
 
 
-    @OnClick(R.id.rbtn_tyj_open)
-    public void rbtn_tyj_open() {
+    @OnClick(R.id.B_rbtn_tyj_open)
+    public void B_rbtn_tyj_open() {
         SerialPortUtil.sendMsg("MBS9");
 
     }
@@ -120,20 +120,20 @@ public class BHomeActivity extends BaseActivity {
     }
 
 
-    @OnClick(R.id.rbtn_tyj_bu_open)
-    public void rbtn_tyj_bu_open() {
+    @OnClick(R.id.B_rbtn_tyj_bu_open)
+    public void B_rbtn_tyj_bu_open() {
         SerialPortUtil.sendMsg("MBS11");
     }
 
-    @OnClick(R.id.rbtn_tyj_bu_close)
-    public void rbtn_tyj_bu_close() {
+    @OnClick(R.id.B_rbtn_tyj_bu_close)
+    public void B_rbtn_tyj_bu_close() {
         SerialPortUtil.sendMsg("MBS12");
     }
 
 
-    @OnClick(R.id.ckbox_kt_open_close)
-    public void ckbox_kt_open_close() {
-        if (ckbox_kt_open_close.isChecked()) {
+    @OnClick(R.id.B_ckbox_kt_open_close)
+    public void B_ckbox_kt_open_close() {
+        if (B_ckbox_kt_open_close.isChecked()) {
             SerialPortUtil.sendMsg("MBS39");
         } else {
             SerialPortUtil.sendMsg("MBS48");
@@ -141,18 +141,18 @@ public class BHomeActivity extends BaseActivity {
     }
 
 
-    @OnClick(R.id.btn_kt_wd_y)
-    public void btn_kt_wd_y() {
+    @OnClick(R.id.B_btn_kt_wd_y)
+    public void B_btn_kt_wd_y() {
         SerialPortUtil.sendMsg("MBS43");
     }
 
-    @OnClick(R.id.btn_kt_wd_n)
-    public void btn_kt_wd_n() {
+    @OnClick(R.id.B_btn_kt_wd_n)
+    public void B_btn_kt_wd_n() {
         SerialPortUtil.sendMsg("MBS44");
     }
 
-    @OnClick(R.id.btn_mj_pc)
-    public void btn_mj_pc() {
+    @OnClick(R.id.B_btn_mj_pc)
+    public void B_btn_mj_pc() {
         SerialPortUtil.sendMsg("MJD46");
     }
 
