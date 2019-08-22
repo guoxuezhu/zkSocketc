99861af161bda10d88484bdf14e109431d547bff
gxz <1174327885@qq.com>
2019-08-22 08:52:41 +0800

    fix UI

--- app/src/main/java/com/lh/zksocketc/ui/SmallActivity.java
+++ app/src/main/java/com/lh/zksocketc/ui/SmallActivity.java
@@ -4,31 +4,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
-import android.widget.CheckBox;
-import android.widget.TextView;
 
-import com.lh.zksocketc.MyApplication;
 import com.lh.zksocketc.R;
-import com.lh.zksocketc.data.DbDao.WsdDataDao;
 import com.lh.zksocketc.utils.ELog;
 import com.lh.zksocketc.utils.SerialPortUtil;
 
-import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class SmallActivity extends BaseActivity {
-
-    @BindView(R.id.tv_wsd_wd)
-    TextView tv_wsd_wd;
-    @BindView(R.id.tv_wsd_sd)
-    TextView tv_wsd_sd;
-    @BindView(R.id.tv_wsd_pm)
-    TextView tv_wsd_pm;
-
-
-    @BindView(R.id.ckbox_kt_open_close)
-    CheckBox ckbox_kt_open_close;
 
     Handler smHander = new Handler() {
         @Override
@@ -37,7 +21,6 @@ public class SmallActivity extends BaseActivity {
             switch (msg.what) {
                 case 567:
                     ELog.e("======smHander====SmallActivity====567=========" + msg.obj.toString());
-                    initView();
                     break;
             }
         }
@@ -49,23 +32,8 @@ public class SmallActivity extends BaseActivity {
         setContentView(R.layout.activity_small);
         ButterKnife.bind(this);
 
-        initView();
-        SerialPortUtil.setWSDmsg(smHander);
     }
 
-    private void initView() {
-        WsdDataDao wsdDataDao = MyApplication.getDaoSession().getWsdDataDao();
-        if (wsdDataDao.loadAll().size() == 0) {
-            tv_wsd_wd.setText("");
-            tv_wsd_sd.setText("");
-            tv_wsd_pm.setText("");
-        } else {
-            tv_wsd_wd.setText(wsdDataDao.loadAll().get(0).wendu);
-            tv_wsd_sd.setText(wsdDataDao.loadAll().get(0).shidu);
-            tv_wsd_pm.setText(wsdDataDao.loadAll().get(0).pm25);
-        }
-
-    }
 
     @OnClick(R.id.rbtn_shangke)
     public void rbtn_shangke() {
@@ -129,25 +97,35 @@ public class SmallActivity extends BaseActivity {
     }
 
 
-    @OnClick(R.id.ckbox_kt_open_close)
-    public void ckbox_kt_open_close() {
-        if (ckbox_kt_open_close.isChecked()) {
-            SerialPortUtil.sendMsg("MBS39");
-        } else {
-            SerialPortUtil.sendMsg("MBS48");
-        }
+    @OnClick(R.id.btn_kt_open)
+    public void btn_kt_open() {
+        SerialPortUtil.sendMsg("MBS39");
     }
 
-
-    @OnClick(R.id.btn_kt_wd_y)
-    public void btn_kt_wd_y() {
-        SerialPortUtil.sendMsg("MBS43");
+    @OnClick(R.id.btn_kt_close)
+    public void btn_kt_close() {
+        SerialPortUtil.sendMsg("MBS48");
     }
 
-    @OnClick(R.id.btn_kt_wd_n)
-    public void btn_kt_wd_n() {
-        SerialPortUtil.sendMsg("MBS44");
+    @OnClick(R.id.btn_xf_open)
+    public void btn_xf_open() {
+        SerialPortUtil.sendMsg("MBS58");
     }
+
+    @OnClick(R.id.btn_xf_close)
+    public void btn_xf_close() {
+        SerialPortUtil.sendMsg("MBS59");
+    }
+
+//    @OnClick(R.id.btn_kt_wd_y)
+//    public void btn_kt_wd_y() {
+//        SerialPortUtil.sendMsg("MBS43");
+//    }
+//
+//    @OnClick(R.id.btn_kt_wd_n)
+//    public void btn_kt_wd_n() {
+//        SerialPortUtil.sendMsg("MBS44");
+//    }
 
     @OnClick(R.id.btn_mj_pc)
     public void btn_mj_pc() {
