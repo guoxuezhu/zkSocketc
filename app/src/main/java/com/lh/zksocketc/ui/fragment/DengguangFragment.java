a90fa2437850e31caaecaf5621c021044bd1e772
gxz <1174327885@qq.com>
2020-12-03 13:53:38 +0800

    fix UI

--- app/src/main/java/com/lh/zksocketc/ui/fragment/DengguangFragment.java
+++ app/src/main/java/com/lh/zksocketc/ui/fragment/DengguangFragment.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import com.lh.zksocketc.R;
 import com.lh.zksocketc.utils.SerialPortUtil;
@@ -32,6 +33,9 @@ public class DengguangFragment extends Fragment {
     @BindView(R.id.rbtn_jsd_close)
     RadioButton rbtn_jsd_close;
 
+    @BindView(R.id.radio_group_dg_all)
+    RadioGroup radio_group_dg_all;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
@@ -45,15 +49,11 @@ public class DengguangFragment extends Fragment {
         SerialPortUtil.sendMsg("MBS13");
         rbtn_hbd_open.setChecked(true);
         rbtn_jsd_open.setChecked(true);
-        rbtn_hbd_close.setChecked(false);
-        rbtn_jsd_close.setChecked(false);
     }
 
     @OnClick(R.id.rbtn_dg_all_close)
     public void rbtn_dg_all_close() {
         SerialPortUtil.sendMsg("MBS14");
-        rbtn_hbd_open.setChecked(false);
-        rbtn_jsd_open.setChecked(false);
         rbtn_hbd_close.setChecked(true);
         rbtn_jsd_close.setChecked(true);
     }
@@ -61,48 +61,36 @@ public class DengguangFragment extends Fragment {
     @OnClick(R.id.rbtn_hbd_open)
     public void rbtn_hbd_open() {
         SerialPortUtil.sendMsg("MBS62");
+        radio_group_dg_all.clearCheck();
         if (rbtn_jsd_open.isChecked()) {
             rbtn_dg_all_open.setChecked(true);
-            rbtn_dg_all_close.setChecked(false);
-        } else {
-            rbtn_dg_all_open.setChecked(false);
-            rbtn_dg_all_close.setChecked(false);
         }
     }
 
     @OnClick(R.id.rbtn_hbd_close)
     public void rbtn_hbd_close() {
         SerialPortUtil.sendMsg("MBS63");
+        radio_group_dg_all.clearCheck();
         if (rbtn_jsd_close.isChecked()) {
-            rbtn_dg_all_open.setChecked(false);
             rbtn_dg_all_close.setChecked(true);
-        } else {
-            rbtn_dg_all_open.setChecked(false);
-            rbtn_dg_all_close.setChecked(false);
         }
     }
 
     @OnClick(R.id.rbtn_jsd_open)
     public void rbtn_jsd_open() {
         SerialPortUtil.sendMsg("MBS64");
+        radio_group_dg_all.clearCheck();
         if (rbtn_hbd_open.isChecked()) {
             rbtn_dg_all_open.setChecked(true);
-            rbtn_dg_all_close.setChecked(false);
-        } else {
-            rbtn_dg_all_open.setChecked(false);
-            rbtn_dg_all_close.setChecked(false);
         }
     }
 
     @OnClick(R.id.rbtn_jsd_close)
     public void rbtn_jsd_close() {
         SerialPortUtil.sendMsg("MBS65");
+        radio_group_dg_all.clearCheck();
         if (rbtn_hbd_close.isChecked()) {
-            rbtn_dg_all_open.setChecked(false);
             rbtn_dg_all_close.setChecked(true);
-        } else {
-            rbtn_dg_all_open.setChecked(false);
-            rbtn_dg_all_close.setChecked(false);
         }
     }
 
