13abbe6ae07b574525f84e5d4bdad50cbc8bcd2b
guoxuezhu <1174327885@qq.com>
2020-12-24 15:44:43 +0800

    fix UI

--- app/src/main/java/com/lh/zksocketc/ui/MainActivity.java
+++ app/src/main/java/com/lh/zksocketc/ui/MainActivity.java
@@ -20,9 +20,12 @@ public class MainActivity extends BaseActivity {
     TextView tv_zh_mm;
     @BindView(R.id.et_new_user)
     EditText et_new_user;
-    @BindView(R.id.et_new_password)
-    EditText et_new_password;
-
+    @BindView(R.id.et_old_password)
+    EditText et_old_password;
+    @BindView(R.id.et_new_password1)
+    EditText et_new_password1;
+    @BindView(R.id.et_new_password2)
+    EditText et_new_password2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,12 +45,28 @@ public class MainActivity extends BaseActivity {
             Toast.makeText(this, "请输入新帐号", Toast.LENGTH_SHORT).show();
             return;
         }
-        if (et_new_password.getText().toString().trim().length() == 0) {
+        if (et_old_password.getText().toString().trim().length() == 0) {
+            Toast.makeText(this, "请输入旧密码", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (et_new_password1.getText().toString().trim().length() == 0) {
             Toast.makeText(this, "请输入新密码", Toast.LENGTH_SHORT).show();
             return;
         }
+        if (et_new_password2.getText().toString().trim().length() == 0) {
+            Toast.makeText(this, "请再次输入新密码", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (!et_old_password.getText().toString().trim().equals(MyApplication.prefs.getMima())) {
+            Toast.makeText(this, "旧密码错误", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (!et_new_password1.getText().toString().trim().equals(et_new_password2.getText().toString().trim())) {
+            Toast.makeText(this, "新旧密码不一致", Toast.LENGTH_SHORT).show();
+            return;
+        }
         MyApplication.prefs.setZhanghao(et_new_user.getText().toString().trim());
-        MyApplication.prefs.setMima(et_new_password.getText().toString().trim());
+        MyApplication.prefs.setMima(et_new_password1.getText().toString().trim());
 
         tv_zh_mm.setText("帐号:" + MyApplication.prefs.getZhanghao() + "        密码:" + MyApplication.prefs.getMima());
         Toast.makeText(this, "修改成功", Toast.LENGTH_SHORT).show();
