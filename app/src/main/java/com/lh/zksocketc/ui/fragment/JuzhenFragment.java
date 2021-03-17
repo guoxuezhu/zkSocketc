fe623337b94811327b1efd2024667538e4a56f5b
guoxuezhu <1174327885@qq.com>
2021-03-17 10:04:09 +0800

    fix

--- app/src/main/java/com/lh/zksocketc/ui/fragment/JuzhenFragment.java
+++ app/src/main/java/com/lh/zksocketc/ui/fragment/JuzhenFragment.java
@@ -4,7 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.CheckBox;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -18,25 +19,17 @@ import butterknife.OnClick;
 
 public class JuzhenFragment extends Fragment {
 
+    @BindView(R.id.radio_group_out)
+    RadioGroup radio_group_out;
 
-//
-//    @BindView(R.id.spbtn_dan_in_gaopaiyi)
-//    CheckBox spbtn_dan_in_gaopaiyi;
-//    @BindView(R.id.spbtn_dan_in_diannao)
-//    CheckBox spbtn_dan_in_diannao;
-//    @BindView(R.id.spbtn_dan_in_bjb_1)
-//    CheckBox spbtn_dan_in_bjb_1;
-//    @BindView(R.id.spbtn_dan_in_bjb_2)
-//    CheckBox spbtn_dan_in_bjb_2;
-//
-//
-//    @BindView(R.id.spbtn_dan_out_touying)
-//    CheckBox spbtn_dan_out_touying;
-//    @BindView(R.id.spbtn_dan_out_dp_1)
-//    CheckBox spbtn_dan_out_dp_1;
-//    @BindView(R.id.spbtn_dan_out_dp_2)
-//    CheckBox spbtn_dan_out_dp_2;
-
+    @BindView(R.id.spbtn_dan_in_lsdn)
+    RadioButton spbtn_dan_in_lsdn;
+    @BindView(R.id.spbtn_dan_in_tsj)
+    RadioButton spbtn_dan_in_tsj;
+    @BindView(R.id.spbtn_dan_in_bjb)
+    RadioButton spbtn_dan_in_bjb;
+    @BindView(R.id.spbtn_dan_in_gpy)
+    RadioButton spbtn_dan_in_gpy;
 
     @Nullable
     @Override
@@ -46,90 +39,77 @@ public class JuzhenFragment extends Fragment {
         return view;
     }
 
-//
-//    @OnClick(R.id.spbtn_dan_in_bjb_2)
-//    public void spbtn_dan_in_bjb_2() {
-//        spbtn_dan_in_gaopaiyi.setChecked(false);
-//        spbtn_dan_in_diannao.setChecked(false);
-//        spbtn_dan_in_bjb_1.setChecked(false);
-//        spbtn_dan_in_bjb_2.setChecked(true);
-//        outBtnClosed();
-//    }
-//
-//    @OnClick(R.id.spbtn_dan_in_diannao)
-//    public void spbtn_dan_in_diannao() {
-//        spbtn_dan_in_gaopaiyi.setChecked(false);
-//        spbtn_dan_in_diannao.setChecked(true);
-//        spbtn_dan_in_bjb_1.setChecked(false);
-//        spbtn_dan_in_bjb_2.setChecked(false);
-//        outBtnClosed();
-//    }
-//
-//
-//    @OnClick(R.id.spbtn_dan_in_bjb_1)
-//    public void spbtn_dan_in_bjb_1() {
-//        spbtn_dan_in_gaopaiyi.setChecked(false);
-//        spbtn_dan_in_diannao.setChecked(false);
-//        spbtn_dan_in_bjb_1.setChecked(true);
-//        spbtn_dan_in_bjb_2.setChecked(false);
-//        outBtnClosed();
-//    }
-//
-//    @OnClick(R.id.spbtn_dan_in_gaopaiyi)
-//    public void spbtn_dan_in_gaopaiyi() {
-//        spbtn_dan_in_gaopaiyi.setChecked(true);
-//        spbtn_dan_in_diannao.setChecked(false);
-//        spbtn_dan_in_bjb_1.setChecked(false);
-//        spbtn_dan_in_bjb_2.setChecked(false);
-//        outBtnClosed();
-//    }
-//
-//
-//    private void outBtnClosed() {
-//        spbtn_dan_out_touying.setChecked(false);
-//        spbtn_dan_out_dp_1.setChecked(false);
-//        spbtn_dan_out_dp_2.setChecked(false);
-//    }
-//
-//
-//    @OnClick(R.id.spbtn_dan_out_touying)
-//    public void spbtn_dan_out_touying() {
-//        spbtn_dan_out_touying.setChecked(true);
-//        spbtn_dan_out_dp_1.setChecked(false);
-//        spbtn_dan_out_dp_2.setChecked(false);
-//        sendDan(1);
-//
-//
-//    }
-//
-//    @OnClick(R.id.spbtn_dan_out_dp_1)
-//    public void spbtn_dan_out_dp_1() {
-//        spbtn_dan_out_touying.setChecked(false);
-//        spbtn_dan_out_dp_1.setChecked(true);
-//        spbtn_dan_out_dp_2.setChecked(false);
-//        sendDan(2);
-//    }
-//
-//
-//    @OnClick(R.id.spbtn_dan_out_dp_2)
-//    public void spbtn_dan_out_dp_2() {
-//        spbtn_dan_out_touying.setChecked(false);
-//        spbtn_dan_out_dp_1.setChecked(false);
-//        spbtn_dan_out_dp_2.setChecked(true);
-//        sendDan(3);
-//    }
-//
-//    private void sendDan(int i) {
-//        if (spbtn_dan_in_gaopaiyi.isChecked()) {
-//            SerialPortUtil.sendMsg("VIDA4," + i);
-//        } else if (spbtn_dan_in_diannao.isChecked()) {
-//            SerialPortUtil.sendMsg("VIDA2," + i);
-//        } else if (spbtn_dan_in_bjb_1.isChecked()) {
-//            SerialPortUtil.sendMsg("VIDA3," + i);
-//        } else if (spbtn_dan_in_bjb_2.isChecked()) {
-//            SerialPortUtil.sendMsg("VIDA1," + i);
-//        }
-//    }
+    @OnClick(R.id.spbtn_all_lsdn)
+    public void spbtn_all_lsdn() {
+        SerialPortUtil.sendMsg("VIDC1");
+    }
 
+    @OnClick(R.id.spbtn_all_tsj)
+    public void spbtn_all_tsj() {
+        SerialPortUtil.sendMsg("VIDC2");
+    }
+
+    @OnClick(R.id.spbtn_all_bjb)
+    public void spbtn_all_bjb() {
+        SerialPortUtil.sendMsg("VIDC3");
+    }
+
+    @OnClick(R.id.spbtn_all_gpy)
+    public void spbtn_all_gpy() {
+        SerialPortUtil.sendMsg("VIDC4");
+    }
+
+
+    @OnClick(R.id.spbtn_dan_in_lsdn)
+    public void spbtn_dan_in_lsdn() {
+        outBtnClosed();
+    }
+
+    @OnClick(R.id.spbtn_dan_in_tsj)
+    public void spbtn_dan_in_tsj() {
+        outBtnClosed();
+    }
+
+    @OnClick(R.id.spbtn_dan_in_bjb)
+    public void spbtn_dan_in_bjb() {
+        outBtnClosed();
+    }
+
+    @OnClick(R.id.spbtn_dan_in_gpy)
+    public void spbtn_dan_in_gpy() {
+        outBtnClosed();
+    }
+
+
+    private void outBtnClosed() {
+        radio_group_out.clearCheck();
+    }
+
+    @OnClick(R.id.spbtn_dan_out_ty)
+    public void spbtn_dan_out_ty() {
+        sendDan(1);
+    }
+
+    @OnClick(R.id.spbtn_dan_out_ping1)
+    public void spbtn_dan_out_ping1() {
+        sendDan(2);
+    }
+
+    @OnClick(R.id.spbtn_dan_out_ping2)
+    public void spbtn_dan_out_ping2() {
+        sendDan(3);
+    }
+
+    private void sendDan(int i) {
+        if (spbtn_dan_in_lsdn.isChecked()) {
+            SerialPortUtil.sendMsg("VIDA1," + i);
+        } else if (spbtn_dan_in_tsj.isChecked()) {
+            SerialPortUtil.sendMsg("VIDA2," + i);
+        } else if (spbtn_dan_in_bjb.isChecked()) {
+            SerialPortUtil.sendMsg("VIDA3," + i);
+        } else if (spbtn_dan_in_gpy.isChecked()) {
+            SerialPortUtil.sendMsg("VIDA4," + i);
+        }
+    }
 
 }
